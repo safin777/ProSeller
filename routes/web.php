243d290fbf917b17eac80e03676c4af589b48003
safin777 <sayedislamsafin7770555@gmail.com>
2021-11-22 22:58:59 +0600

    jetstream installed in this project

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeComponent::class)->name('home');
 
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
